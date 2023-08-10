4db63f3ba6e269c63a50811c4b02ea529228513d
Nhyira <nhyiratnkansah@gmail.com>
2023-08-10 13:18:18 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var facts = ["I love photography", "I am an athlete", "I am a music connoisseur"]
+    @State private var factDisplay
+    
+    var body: some View {
+        VStack {
+            Text("Nhyira Nkansah")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.836, saturation: 0.662, brightness: 0.894))
+            
+            Image ("aboutMe_9490")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .padding([.top, .leading, .bottom], 19.0)
+            
+            Spacer()
+                .frame(width: 5.0, height: 25.0)
+            
+            
+            Button("Click for facts") {
+                for fact in facts {
+                    Text(fact)
+                }
+                
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint((Color(hue: 0.833, saturation: 0.297, brightness: 0.965)))
+            
+            Text("I love photography and music")
+                .foregroundColor(Color.black)
+                
+            
+
+            
+            
+                
+        }
+        .padding(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
